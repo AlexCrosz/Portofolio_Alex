1fa67ea5cce8ecf4965b184f50418ba1889b0333
AlexCrosz <alex26gurren@gmail.com>
2025-07-31 17:54:34 +0700

    V 2.0 Baru

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Portofolio_Alex/', // Ganti sesuai nama repo kamu
   plugins: [react()],
 });
